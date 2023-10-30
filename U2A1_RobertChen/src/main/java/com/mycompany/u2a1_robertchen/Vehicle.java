e87c944f390b5bc14411eae322bcb8e25dc0eaf5
335091559 <335091559@C-2UA4162GS4.tdsb.on.ca>
2023-10-30 11:41:42 -0400

    Created the java classes

---
+++ U2A1_RobertChen/src/main/java/com/mycompany/u2a1_robertchen/Vehicle.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.u2a1_robertchen;
+
+/**
+ *
+ * @author 335091559
+ */
+public class Vehicle {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
